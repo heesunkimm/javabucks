39f7654b80cab19fc391c5f468f457c90aea90d7
heesunkimm <khsun4624@naver.com>
2024-10-27 21:04:43 +0900

    [수정][오류수정]

--- javabucks_store/src/main/java/com/project/javabucksStore/JavabucksApplication.java
+++ javabucks_store/src/main/java/com/project/javabucksStore/JavabucksApplication.java
@@ -5,17 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class JavabucksApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavabucksApplication.class, args);
 	}
-
-	@SpringBootApplication
-	@EnableScheduling public class SchedulerApplication {
-	 public static void main(String[] args) {
-		 SpringApplication.run(SchedulerApplication.class, args); }
-	 }
-
 
 }
